6424178419b4a16f4744947643d95597fcb52a8e
Lars Jaatun <lars@atilf.no>
2024-03-07 17:05:21 +0100

    add store and very basic form to website

---
+++ client/src/components/input_form.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { onUpdated, ref, } from 'vue'
+import { useFormStore } from '../stores/form_store.js'
+
+const form_store = useFormStore()
+const distance_traveled = ref(0);
+const payload_weight = ref(0);
+
+
+
+onUpdated(() =>{console.log(distance_traveled.value)
+update_values_in_store()
+})
+
+function update_values_in_store() {
+  form_store.update_form_store(distance_traveled.value, payload_weight.value)
+}
+</script>
+
+<template>
+
+  <form>
+    <label for="distance_traveled">Distance traveled</label>
+    <input type="number" id="distance_traveled"v-model="distance_traveled" placeholder="100"/>
+    <label for="payload_weight">Payload weight</label>
+    <input type="number" id="payload_weight" v-model="payload_weight" placeholder="400"/>
+  </form>
+
+<h1>{{distance_traveled}}</h1>
+
+</template>
+
+<style scoped>
+
+
+</style>
