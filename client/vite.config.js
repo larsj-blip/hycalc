2374663cbdc1ee77855f40994cbf9bf837452dc6
Lars Jaatun <lars@atilf.no>
2024-03-13 16:37:41 +0100

    serving from flask

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,6 +8,9 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  build: {
+    assetsDir: "static",
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
