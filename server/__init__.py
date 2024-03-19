ad66f90f6f221ac7bceee74a9c1713a5ec55f1e5
Lars Jaatun <lars@atilf.no>
2024-03-19 23:00:26 +0100

    select different static folder

--- server/__init__.py
+++ server/__init__.py
@@ -6,7 +6,7 @@ from flask import Flask, render_template
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True,
-                static_folder="../client/dist/static", template_folder="../client/dist/")
+                static_folder="static/static", template_folder="static")
     app.config.from_mapping(
         SECRET_KEY='dev'
     )
