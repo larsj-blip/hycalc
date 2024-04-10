99559e0102f46e13aa6ea079f52276ac2b6d4fe1
Lars Jaatun <lars@atilf.no>
2024-04-10 19:51:19 +0200

    edit route to avoid error in production

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -18,13 +18,13 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => ResultsView
+      component: ResultsView
     },
 
     {
       path: '/sources',
       name: 'sources',
-      component: () => Sources
+      component: Sources
     }
   ]
 })
