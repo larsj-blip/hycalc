cda66487dbf7cb72b649b2442d2d863d1069ec11
lars jaatun <very.lars@gmail.com>
2024-04-23 20:12:44 +0200

    add bibtex from funny website as iframe

--- client/src/views/SourcesView.vue
+++ client/src/views/SourcesView.vue
@@ -1,66 +1,22 @@
 <script setup>
+import {onMounted} from "vue";
 
+// onMounted(() => {
+//   let bibtexScript = document.createElement('script')
+//   bibtexScript.setAttribute('src', 'https://cdn.jsdelivr.net/gh/pcooksey/bibtex-js@1.0.0/src/bibtex_js.min.js');
+//   document.head.appendChild(bibtexScript)
+// })
 </script>
+
 <template>
-    <div class="sources1">
-      <h2>Sources for calculating hydrogen price</h2>
-      <ul>
-        <li>Werner Zittel; Reinhold Wurster (1996-07-08). "Chapter 3: Production of Hydrogen. Part 4: Production from electricity by means of electrolysis".</li>
-        <li>Bjørnar Kruse; Sondre Grinna; Cato Buch (2002-02-13). "Hydrogen – Status and Possibilities". The Bellona Foundation</li>
-        <li> realto.io: Connect to spot market price electricity across Europe </li>
-
-      </ul>
-    </div>
-
-
-    <div class="sources2">
-      <h2>Sources for  calculating number of refuels</h2>
-      <ul>
-        <li> </li>
-        <li></li>
-        <!-- Add more sources as needed -->
-      </ul>
-    </div>
-
-
-    <div class="sources3">
-      <h2>Sources for calculating minutes spent refueling</h2>
-      <ul>
-        <li> </li>
-        <li></li>
-      </ul>
-    </div>
+<iframe src="https://bibbase.org/show?bib=https%3A%2F%2Fbibbase.org%2Fnetwork%2Ffiles%2F6xNSEj66QjQ799Kdy&noBootstrap=1"></iframe>
 
 </template>
 
 <style scoped>
-.content {
-  /* Your styling for the content section */
-}
-
-.sources1 {
-  margin-top: 50px; /* Adjust margin as needed */
-}
-
-.sources1 h2 {
-  font-size: 1.5em; /* Adjust font size as needed */
-}
-
-.sources ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-.sources1 li {
-  margin-bottom: 10px; /* Adjust margin as needed */
-}
-
-.sources1 a {
-  text-decoration: none;
-  color: #007bff; /* Link color */
-}
-
-.sources1 a:hover {
-  text-decoration: underline;
+iframe {
+  display: flex;
+  margin: auto auto;
+  width: 80%;
 }
 </style>
